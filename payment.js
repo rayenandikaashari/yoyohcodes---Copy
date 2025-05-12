47c20e2503edb8f1ff1a7eb2252ddec91cb4c1c6
rayenandikaashari <2310631170043@student.unsika.ac.id>
2025-05-12 16:34:01 +0700

    Barang Menghilang

--- payment.js
+++ payment.js
@@ -1,4 +1,6 @@
 function payWithCash() {
+    localStorage.removeItem('cart');
+    
     alert('Anda memilih untuk membayar menggunakan cash. Terima kasih!');
     window.location.href = 'ayobelanjalagi.html'; // Navigasi ke halaman baru
 }
